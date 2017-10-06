a8689cc89f89614bde68907e13f88615af8762db
Finbar Crago <finbar.crago@gmail.com>
2017-10-07 00:49:50 +1100

    rename x,y --> row,col

--- knight.c
+++ knight.c
@@ -3,11 +3,11 @@
 
 #define SIZE 8
 
-#define GET_Y(b,ptr)   (((unsigned long)ptr-(unsigned long)&b)/sizeof(b[0][0])%(sizeof(b[0])/sizeof(b[0][0])))
-#define GET_X(b,ptr,y) (((((unsigned long)ptr-(unsigned long)&b)/sizeof(b[0][0])-y))/(sizeof(b[0])/sizeof(b[0][0])))
+#define GET_COL(b,ptr)   (((unsigned long)ptr-(unsigned long)&b)/sizeof(b[0][0])%(sizeof(b[0])/sizeof(b[0][0])))
+#define GET_ROW(b,ptr,col) (((((unsigned long)ptr-(unsigned long)&b)/sizeof(b[0][0])-col))/(sizeof(b[0])/sizeof(b[0][0])))
 
-struct {int x,y;} M[8] = {{ 1, 2}, { 2, 1}, { 2,-1}, { 1,-2}, {-1,-2}, {-2,-1}, {-2, 1}, {-1, 2}};
-#define SAFE(n, x, y) ( (x+M[n].x>=0) && (x+M[n].x<SIZE) && (y+M[n].y>=0) && (y+M[n].y<SIZE) )
+struct {int row,col;} M[8] = {{ 1, 2}, { 2, 1}, { 2,-1}, { 1,-2}, {-1,-2}, {-2,-1}, {-2, 1}, {-1, 2}};
+#define SAFE(n, row, col) ( (row+M[n].row>=0) && (row+M[n].row<SIZE) && (col+M[n].col>=0) && (col+M[n].col<SIZE) )
 
 struct mov {
 	int n;
@@ -29,7 +29,7 @@ int main(int argc, char **argv){
 	best = 0;
 
 	do {
-		int i, x, y;
+		int i, row, col;
 		move = stack[sptr--].mov;
 
 		if(!move)
@@ -38,13 +38,13 @@ int main(int argc, char **argv){
 		move->n    = stack[sptr+1].from->n + 1;
 		move->from = stack[sptr+1].from;
 
-		y = GET_Y(board, move);
-		x = GET_X(board, move, y);
+		col = GET_COL(board, move);
+		row = GET_ROW(board, move, col);
 
 		for(i=0; i < 8; i++){
-			if( SAFE(i, x, y) && !board[x+M[i].x][y+M[i].y].n ){
+			if( SAFE(i, row, col) && !board[row+M[i].row][col+M[i].col].n ){
 				sptr++;
-				stack[sptr].mov  = &board[x+M[i].x][y+M[i].y];
+				stack[sptr].mov  = &board[row+M[i].row][col+M[i].col];
 				stack[sptr].from = move;
 			}
 		}
@@ -54,16 +54,16 @@ int main(int argc, char **argv){
 		if(move->n > best){
 			best = move->n;
 
-			printf("[MOV:%2d; S:%2d] (%d, %d)\n", move->n, sptr, x, y);
+			printf("[MOV:%2d; S:%2d] (%d, %d)\n", move->n, sptr, row, col);
 
-			for(x=0;x<SIZE;x++){
+			for(row=0;row<SIZE;row++){
 				printf("|");
-				for(y=0;y<SIZE;y++){
+				for(col=0;col<SIZE;col++){
 					printf("%s%s%s%3d\x1b[0m|",
-						   (&board[x][y] == move)?"\x1b[1;32m":"",
-						   (&board[x][y] == move->from)?"\x1b[36m":"",
-						   (!board[x][y].n)?"\x1b[31;41m":"",
-						   board[x][y].n);
+						   (&board[row][col] == move)?"\x1b[1;32m":"",
+						   (&board[row][col] == move->from)?"\x1b[36m":"",
+						   (!board[row][col].n)?"\x1b[31;41m":"",
+						   board[row][col].n);
 				}
 				printf("\n");
 			}
@@ -75,8 +75,8 @@ int main(int argc, char **argv){
 				if(move == &board[0][0])
 					break;
 
-				y = GET_Y(board, move);
-				x = GET_X(board, move, y);
+				col = GET_COL(board, move);
+				row = GET_ROW(board, move, col);
 
 				move->n = 0;
 				move = move->from;
