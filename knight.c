b0b3c8a1406b384c06977e46ad8aa4eacc16c44f
Finbar Crago <finbar.crago@gmail.com>
2017-10-08 23:28:17 +1100

    sort working!

--- knight.c
+++ knight.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 #ifndef SIZE
 #define SIZE 8
@@ -7,13 +8,35 @@
 struct mov { int n;	struct mov *from; };
 
 struct mov board[SIZE][SIZE];
-struct {struct mov *mov; struct mov *from;} stack[SIZE*SIZE*SIZE];
+struct stack {struct mov *mov; struct mov *from;} stack[SIZE*SIZE*SIZE];
 
 struct {int row,col;} M[8] = {{ 1, 2}, { 2, 1}, { 2,-1}, { 1,-2}, {-1,-2}, {-2,-1}, {-2, 1}, {-1, 2}};
 
-#define SAFE(n, row, col) ( (row+M[n].row>=0) && (row+M[n].row<SIZE) && (col+M[n].col>=0) && (col+M[n].col<SIZE) )
+#define SAFE(n,r,c) ((r+M[n].row>=0) && (r+M[n].row<SIZE) && (c+M[n].col>=0) && (c+M[n].col<SIZE))
 #define GET_COL(b,ptr) (((unsigned long)ptr-(unsigned long)&b)/sizeof(b[0][0])%(sizeof(b[0])/sizeof(b[0][0])))
 #define GET_ROW(b,ptr,col) (((((unsigned long)ptr-(unsigned long)&b)/sizeof(b[0][0])-col))/(sizeof(b[0])/sizeof(b[0][0])))
+
+static int mov_comp(const void *a, const void *b){
+	int i, row_a, col_a, row_b, col_b, mov_a, mov_b;
+
+	col_a = GET_COL(board, ((struct stack *)a)->mov );
+	col_b = GET_COL(board, ((struct stack *)b)->mov);
+	row_a = GET_ROW(board, ((struct stack *)a)->mov, col_a);
+	row_b = GET_ROW(board, ((struct stack *)b)->mov, col_b);
+
+	mov_a = 0;
+	mov_b = 0;
+
+	for(i=0; i < 8; i++){
+		if(SAFE(i, row_a, col_a) && !board[row_a+M[i].row][col_a+M[i].col].n)
+			mov_a++;
+
+		if(SAFE(i, row_b, col_b) && !board[row_b+M[i].row][col_b+M[i].col].n)
+			mov_b++;
+	}
+
+	return (mov_a < mov_b);
+}
 
 int main(int argc, char **argv){
 	struct mov *move;
@@ -27,7 +50,7 @@ int main(int argc, char **argv){
 	best = 0;
 
 	do {
-		int i, row, col;
+		int i, new, row, col;
 
 		move = stack[sptr--].mov;
 		move->n    = stack[sptr+1].from->n + 1;
@@ -36,17 +59,22 @@ int main(int argc, char **argv){
 		col = GET_COL(board, move);
 		row = GET_ROW(board, move, col);
 
+		new = 0;
 		for(i=0; i < 8; i++){
 			if( SAFE(i, row, col) && !board[row+M[i].row][col+M[i].col].n ){
 				sptr++;
 				stack[sptr].mov  = &board[row+M[i].row][col+M[i].col];
 				stack[sptr].from = move;
+				new++;
 			}
 		}
 
-		/* tree should be symmetrical so we can discard the first branch */
-		if(move == &board[0][0])
-			sptr--;
+		/* reorder the new stack elements by lowest moves */
+		qsort(&stack[sptr-new+1], new, sizeof(struct stack), mov_comp);
+
+
+		/* tree should be symmetrical so we can discard the first branch? */
+		/* if(move == &board[0][0])	sptr--;	*/
 
 		if(move->n > best){
 			best = move->n;
